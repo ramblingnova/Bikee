befd55d7d7e57304ba266291112babf68955002b
ramblingnova <slless_des@naver.com>
2015-11-17 21:03:03 +0900

    12th

--- app/src/main/java/com/example/tacademy/bikee/renter/sidemenu/evaluatingbicycle/EvaluatingBicyclePostScriptListActivity.java
+++ app/src/main/java/com/example/tacademy/bikee/renter/sidemenu/evaluatingbicycle/EvaluatingBicyclePostScriptListActivity.java
@@ -3,10 +3,22 @@ package com.example.tacademy.bikee.renter.sidemenu.evaluatingbicycle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.tacademy.bikee.R;
+import com.example.tacademy.bikee.etc.dao.Comment;
+import com.example.tacademy.bikee.etc.dao.ReceiveObject;
+import com.example.tacademy.bikee.etc.dao.Result;
+import com.example.tacademy.bikee.etc.manager.NetworkManager;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class EvaluatingBicyclePostScriptListActivity extends AppCompatActivity {
 
@@ -28,9 +40,30 @@ public class EvaluatingBicyclePostScriptListActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        for(int i = 0; i < 10; i++) {
-            adapter.add("" + i, "" + i, i, "" + i, "" + i);
-        }
+        // 내평가보기
+        NetworkManager.getInstance().selectUserComment(new Callback<ReceiveObject>() {
+            @Override
+            public void success(ReceiveObject receiveObject, Response response) {
+                Log.i("result", "onResponse Success");
+                List<Result> results = receiveObject.getResult();
+                for (Result result : results)
+                    for (Comment comment : result.getComments()) {
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM.dd HH:mm");
+                        Log.i("result", "onResponse Id : " + comment.get_id()
+                                + ", Writer Name : " + result.getBike().getTitle()
+                                + ", CreateAt : " + simpleDateFormat.format(comment.getCreatedAt())
+                                + ", Body : " + comment.getBody()
+                                + ", Point : " + comment.getPoint()
+                        );
+                        adapter.add(result.getBike().getTitle(), simpleDateFormat.format(comment.getCreatedAt()), comment.getBody(), comment.getPoint());
+                    }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("error", "onFailure Error : " + error.toString());
+            }
+        });
     }
 
     @Override
