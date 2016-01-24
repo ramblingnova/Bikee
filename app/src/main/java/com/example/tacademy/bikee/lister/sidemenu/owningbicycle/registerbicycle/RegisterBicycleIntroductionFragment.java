16d4b207ec5a0de4759c6bd0330621f8afe74f33
ramblingnova <slless_des@naver.com>
2016-01-24 23:27:02 +0900

    34th

--- app/src/main/java/com/example/tacademy/bikee/lister/sidemenu/owningbicycle/registerbicycle/RegisterBicycleIntroductionFragment.java
+++ app/src/main/java/com/example/tacademy/bikee/lister/sidemenu/owningbicycle/registerbicycle/RegisterBicycleIntroductionFragment.java
@@ -1,21 +1,31 @@
 package com.example.tacademy.bikee.lister.sidemenu.owningbicycle.registerbicycle;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.example.tacademy.bikee.R;
 import com.example.tacademy.bikee.etc.manager.FontManager;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class RegisterBicycleIntroductionFragment extends Fragment {
-    private EditText nameEditText;
-    private EditText introductionEditText;
+    @Bind(R.id.fragment_register_bicycle_introduction_bicycle_name)
+    EditText nameEditText;
+    @Bind(R.id.fragment_register_bicycle_introduction_bicycle_introduction)
+    EditText introductionEditText;
+    private InputMethodManager imm;
 
     public static RegisterBicycleIntroductionFragment newInstance() {
         return new RegisterBicycleIntroductionFragment();
@@ -26,10 +36,11 @@ public class RegisterBicycleIntroductionFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register_bicycle_introduction, container, false);
 
-        nameEditText = (EditText)view.findViewById(R.id.fragment_register_bicycle_introduction_bicycle_name);
-        introductionEditText = (EditText)view.findViewById(R.id.fragment_register_bicycle_introduction_bicycle_introduction);
+        ButterKnife.bind(this, view);
+
         nameEditText.addTextChangedListener(tw);
         introductionEditText.addTextChangedListener(tw);
+        imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
         return view;
     }
@@ -58,6 +69,21 @@ public class RegisterBicycleIntroductionFragment extends Fragment {
 
         }
     };
+
+    @OnClick({R.id.fragment_register_bicycle_introduction_bicycle_name_layout,
+            R.id.fragment_register_bicycle_introduction_bicycle_introduction_layout})
+    void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fragment_register_bicycle_introduction_bicycle_name_layout:
+                nameEditText.requestFocus();
+                imm.showSoftInput(nameEditText, InputMethodManager.SHOW_FORCED);
+                break;
+            case R.id.fragment_register_bicycle_introduction_bicycle_introduction_layout:
+                introductionEditText.requestFocus();
+                imm.showSoftInput(introductionEditText, InputMethodManager.SHOW_FORCED);
+                break;
+        }
+    }
 
     public String getName() {
         return nameEditText.getText().toString();
