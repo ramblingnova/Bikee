8053552a22f4ec0227040e7d9a1a182454846e82
ramblingnova <slless_des@naver.com>
2016-04-28 04:47:04 +0900

    43th

---
+++ ArsenalCalendarLibrary/src/main/java/com/wdullaer/materialdatetimepicker/date/OnDateSelectedListener.java
@@ -0,0 +1,12 @@
+package com.wdullaer.materialdatetimepicker.date;
+
+import android.view.View;
+
+import java.util.Date;
+
+/**
+ * Created by User on 2016-04-22.
+ */
+public interface OnDateSelectedListener {
+    void onDateSelected(View view, Date date);
+}
